04bc096389872a8d0f47f883ddbff71581b8dbfb
Jan <jan.wendling@gmail.com>
2023-01-15 17:29:19 +0100

    Added PDF Splitter script

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# pdf_splitter
-Simple python tool to split a PDF page into multiple pages
+# PDF Splitter
+
+I wrote this tiny script to split one PDF page into multiple pages.
+
+## Getting Started
+
+1. Install the dependencies by running `pip install -r requirements.txt`.
+2. Copy the PDF that needs to be splitted into the same directory as the script
+3. Run the application `python pdf_splitter.py [-h] filename pages`
+
+The pages are a comma-separated list of page numbers to identify the first page of each new PDF file.
+E.g. You have a PDF with 10 pages and you want the new pages to contain
+1. 1st PDF page 1-2
+2. 2nd PDF page 3-6
+3. 3rd PDF page 7-10
+
+Then the command is `python pdf_splitter.py FILENAME 1,3,7
+
+## Have fun!
---
+++ pdf_splitter.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import argparse
+import pathlib
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+# Method to split the pdf at every given n pages.
+def split_pdf(filename: str, pages: str):
+
+
+    filepath = pathlib.Path(filename)
+    new_filename = filename[:-4]
+
+    input_file = open(filepath, 'rb')
+    pdf_org = PdfFileReader(input_file)
+
+    # Get pages
+    new_first_pages = [int(i) for i in pages.split(',')]
+    
+    new_file = None
+    pdf_page_count = pdf_org.getNumPages()
+    for i in range(pdf_page_count):
+        if i+1 in new_first_pages:
+            if new_file is not None:
+                # write to file
+                with open(f"{new_filename}_{i}.pdf", "wb") as output_stream:
+                    new_file.write(output_stream)
+            new_file = PdfFileWriter()
+
+        new_file.addPage(pdf_org.getPage(i))
+
+    # write last file
+    with open(f"{new_filename}_{i}.pdf", "wb") as output_stream:
+        new_file.write(output_stream)
+
+if __name__ == "__main__":
+    # Local execution
+    parser = argparse.ArgumentParser(prog = 'pdf_splitter.py', description = 'Takes a list of pages and splits a PDF')
+
+    parser.add_argument('filename')
+    parser.add_argument('pages', help="Pages as comma separated list. The number indicates a new PDF file.")
+
+    args = parser.parse_args()
+
+    split_pdf(args.filename, args.pages)
